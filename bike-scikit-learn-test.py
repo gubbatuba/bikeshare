be48b437bf9bf7232e04ff6ee189cc60cfb4ac21
Subhash Gubba <sgubba@SHUML.(none)>
2014-11-04 19:09:00 -0500

    bike code

--- bike-scikit-learn-test.py
+++ bike-scikit-learn-test.py
@@ -1,1 +1,13 @@
-	import sklearn
+print(__doc__)
+
+
+# Code source: Jaques Grobler
+# License: BSD 3 clause
+
+
+import pylab as pl
+import numpy*
+from sklearn import datasets, linear_model
+
+X = genfromtxt('dataset1.csv', delimiter=',')
+Y = genfromtxt('output.csv', delimiter=',')
