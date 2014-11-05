d31dd60a6277a2228e4862cefa763597a29b1abc
Subhash Gubba <sgubba@SHUML.(none)>
2014-11-04 19:24:27 -0500

    next iteration

--- bike-scikit-learn-test.py
+++ bike-scikit-learn-test.py
@@ -1,9 +1,16 @@
 import pylab as pl
 from numpy import *
-from sklearn import datasets, linear_model
+from sklearn import datasets, linear_model, cross_validation
 
 X = genfromtxt('dataset1.csv', delimiter=',')
 Y = genfromtxt('output.csv', delimiter=',')
 
 print(X)
 print(Y)
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, Y, test_size=0.4, random_state=0)
+
+clf = linear_model.Ridge (alpha = 1000)
+clf.fit (X_train,y_train)
+print(clf.score(X_test,y_test))
+
